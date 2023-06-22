14657d5189936608e78e64aa5639fbb48e54ac3a
alibabassaud <132604706+alibabassaud@users.noreply.github.com>
2023-06-22 11:00:39 -0300

    Update image.tsx

--- src/pages/image.tsx
+++ src/pages/image.tsx
@@ -61,7 +61,7 @@ export default function Home() {
             >
                 <div className="p-3">
                 <Image
-                    src="/tc-image.png"
+                    src="/tc-fake.jpg"
                     width={500}
                     height={500}
                     alt="Picture of the author"
